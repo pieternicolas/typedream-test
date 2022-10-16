04dd513cca7790cb2b5bb34c8343d5e9fb2f84a3
Nicolas Pieter <nicolaspieter@yahoo.com>
2022-10-16 23:56:09 +0700

    Fix problem with ssr and local storage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,5 @@
-export { default } from 'screens/Home';
+import dynamic from 'next/dynamic';
+
+const Home = dynamic(() => import('screens/Home'), { ssr: false }); // This is to prevent errors from using local storage as a storing medium
+
+export default Home;
